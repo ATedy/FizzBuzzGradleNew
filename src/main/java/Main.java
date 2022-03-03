3fd08bcc6c97a0355814c2cb894f2531c1e6d805
Amanuel <amanuel.tedros@capgemini.com>
2022-03-03 14:59:58 +0000

    application plugin added for running all classes from main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ import static com.buildExample.FizzBuzzJson.fizzBuzzJson;
 public class Main {
     public static void main(String[] args) {
         fizzBuzzJson(89, 10);
-        fizzBuzzInputs();
         fizzBuzzArguments(300, 100);
+//        fizzBuzzInputs();
+
     }
 }
